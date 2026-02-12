b2d4560c8933b99a1994bf8a4561d971e29efa4c
Anobhav Singh <singhanobhav@gmail.com>
2026-02-13 01:23:46 +0530

    reactjs

---
+++ ReactJS/routering_react/src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Layout from './Layout.jsx'
+import About from './components/Aboutus/Aboutus.jsx'
+import Home from './components/Home/Home.jsx'
+import User from './components/User/User.jsx'  // ✅ make sure path is correct
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: 'user/:userid',   // ✅ correct way
+        element: <User />
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
